c79c17f8be146c02efbe4c8e4c1455937a98c8ad
tomga1 <tomasgarcia003@gmail.com>
2025-05-26 23:48:33 -0300

    agregue mockup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import BtnVerde from './components/BtnVerde'
+import DeviceMockup from './components/DeviceMockup'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <DeviceMockup />
     </>
   )
 }
